57887f9db48f1cda4982c7769ebf081cee2377f9
Santanu Pal <santanup@capitalnumbers.com>
2018-11-29 18:21:45 +0530

    Working on Charts for dashboard Santanu

--- app/src/main/java/com/surefiz/screens/dashboard/DashBoardActivity.java
+++ app/src/main/java/com/surefiz/screens/dashboard/DashBoardActivity.java
@@ -3,7 +3,28 @@ package com.surefiz.screens.dashboard;
 import android.os.Bundle;
 import android.view.View;
 
+import com.highsoft.highcharts.common.HIColor;
+import com.highsoft.highcharts.common.hichartsclasses.HICSSObject;
+import com.highsoft.highcharts.common.hichartsclasses.HIChart;
+import com.highsoft.highcharts.common.hichartsclasses.HICondition;
+import com.highsoft.highcharts.common.hichartsclasses.HILabel;
+import com.highsoft.highcharts.common.hichartsclasses.HILegend;
+import com.highsoft.highcharts.common.hichartsclasses.HILine;
+import com.highsoft.highcharts.common.hichartsclasses.HIOptions;
+import com.highsoft.highcharts.common.hichartsclasses.HIPlotOptions;
+import com.highsoft.highcharts.common.hichartsclasses.HIResponsive;
+import com.highsoft.highcharts.common.hichartsclasses.HIRules;
+import com.highsoft.highcharts.common.hichartsclasses.HISeries;
+import com.highsoft.highcharts.common.hichartsclasses.HISubtitle;
+import com.highsoft.highcharts.common.hichartsclasses.HITitle;
+import com.highsoft.highcharts.common.hichartsclasses.HIYAxis;
+import com.highsoft.highcharts.core.HIChartView;
 import com.surefiz.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class DashBoardActivity extends BaseActivity {
 
@@ -14,8 +35,105 @@ public class DashBoardActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         view = View.inflate(this, R.layout.activity_dash_board, null);
         addContentView(view);
+        showChart();
         setHeaderView();
     }
+
+    private void showChart() {
+        HIChartView chartView = (HIChartView) findViewById(R.id.hc);
+
+        HIOptions options = new HIOptions();
+
+        HIChart chart = new HIChart();
+        chart.setType("line");
+        chart.setBackgroundColor(HIColor.initWithHexValue("#000000"));
+        options.setChart(chart);
+
+        HITitle title = new HITitle();
+        title.setText("Solar Employment Growth by Sector, 2010-2016");
+        options.setTitle(title);
+
+        HISubtitle subtitle = new HISubtitle();
+        subtitle.setText("Source: thesolarfoundation.com");
+        options.setSubtitle(subtitle);
+
+        HIYAxis yaxis = new HIYAxis();
+        HITitle title1 = new HITitle();
+        title1.setText("Number of Employees");
+        yaxis.setTitle(title1);
+        options.setYAxis(new ArrayList<>(Collections.singletonList(yaxis)));
+
+        HILegend legend = new HILegend();
+        legend.setLayout("vertical");
+        legend.setAlign("right");
+        legend.setVerticalAlign("middle");
+        options.setLegend(legend);
+
+        HIPlotOptions plotoptions = new HIPlotOptions();
+        HISeries hiSeries = new HISeries();
+        hiSeries.setPointStart(2010);
+        HILabel hiLabel = new HILabel();
+        hiSeries.setLabel(hiLabel);
+        hiLabel.setConnectorAllowed(false);
+        plotoptions.setSeries(hiSeries);
+
+        HILine line1 = new HILine();
+        line1.setName("Installation");
+        line1.setData(new ArrayList<>(Arrays.asList(43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175)));
+
+        HILine line2 = new HILine();
+        line2.setName("Manufacturing");
+        line2.setData(new ArrayList<>(Arrays.asList(24916, 24064, 29742, 29851, 32490, 30282, 38121, 404340)));
+
+        HILine line3 = new HILine();
+        line3.setName("Sales & Distribution");
+        line3.setData(new ArrayList<>(Arrays.asList(11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387)));
+
+        HILine line4 = new HILine();
+        line4.setName("Project Development");
+        line4.setData(new ArrayList<>(Arrays.asList(null, null, 7988, 12169, 15112, 22452, 34400, 34227)));
+
+        HILine line5 = new HILine();
+        line5.setName("Other");
+        ;
+        line5.setData(new ArrayList<>(Arrays.asList(12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111)));
+
+        HIResponsive responsive = new HIResponsive();
+
+        HIRules rules1 = new HIRules();
+        HICondition hiCondition = new HICondition();
+        hiCondition.setMaxWidth(500);
+        rules1.setCondition(hiCondition);
+        HashMap<String, HashMap> chartLegend = new HashMap<>();
+        HashMap<String, String> legendOptions = new HashMap<>();
+        legendOptions.put("layout", "horizontal");
+        legendOptions.put("align", "center");
+        legendOptions.put("verticalAlign", "bottom");
+        chartLegend.put("legend", legendOptions);
+        rules1.setChartOptions(chartLegend);
+        responsive.setRules(new ArrayList<>(Collections.singletonList(rules1)));
+        options.setResponsive(responsive);
+
+        options.setSeries(new ArrayList<>(Arrays.asList(line1, line2, line3, line4, line5)));
+
+        chartView.setOptions(options);
+
+        /*HIChart chart = new HIChart();
+        chart.setType("column");
+        options.setChart(chart);
+
+        HITitle title = new HITitle();
+        title.setText("Demo chart");
+
+        options.setTitle(title);
+
+        HIColumn series = new HIColumn();
+        series.setData(new ArrayList<>(Arrays.asList(49.9, 71.5, 106.4, 129.2, 144, 176, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4)));
+        options.setSeries(new ArrayList<HISeries>(Collections.singletonList(series)));
+
+        chartView.setOptions(options);*/
+    }
+
 
     private void setHeaderView() {
         iv_edit.setVisibility(View.GONE);

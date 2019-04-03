2138b79868174c46488e02acedb2fffef7ad8393
Dimpi Singh <dimpi@capitalnumbers.com>
2019-04-03 17:40:19 +0530

    please choose email and name popup

--- app/src/main/java/com/surefiz/dialog/AddUserDialog.java
+++ app/src/main/java/com/surefiz/dialog/AddUserDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
